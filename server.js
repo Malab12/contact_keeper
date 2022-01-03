067978789ee2177b7b8341687b3920e2133c06e6
unknown <malab@outlook.com>
2022-01-03 20:12:10 +0530

    Created User model and password hashing

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to the database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact-keeper-api'}));
 
 // Define Routers
