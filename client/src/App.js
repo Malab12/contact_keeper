7b147dcd8a05642754e1d1eae6b4530b1d6e2b87
unknown <malab@outlook.com>
2022-01-09 20:41:12 +0530

    Added Login and Register Pages and alert function

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,39 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-      <Fragment className="App">
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='about' element={<About />} />
-          </Routes>
-        </div>
-      </Fragment>
-    </Router>
-    </ContactState>    
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+          <Fragment className="App">
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/register' element={<Register />} />
+                  <Route path='/login' element={<Login />} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>  
+    </AuthState>
+      
   );
 }
 
